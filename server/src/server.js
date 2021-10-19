e45017e21929e9a63d9df397ac1483bba6322c5d
Thiago Costa <thiago.costa@livsaude.com.br>
2021-10-19 16:39:53 -0300

    init server

---
+++ server/src/server.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server, { wsEngine: 'ws' })
+
+app.use(cors())
+
+io.on('connection', socket => {
+    socket.on('join', ({ name, room }) => {
+        const { error, user } = addUser({ id: socket.id, name, room })
+        
+        if (error) {
+            return;
+        }
+        console.log('joined')
+
+        socket.emit('message', {
+            user: 'admin',
+            text: `${user.name}, welcome to the room`
+        })
+
+        socket.broadcast.to(user.room).emit('message', {
+            user: 'admin',
+            text: `${user.name} has joined to room.`,
+        })
+
+        socket.join(user.room);
+
+		io.to(user.room).emit("roomData", {
+			room: user.room,
+			users: getUsersInRoom(user.room),
+		});
+    })
+
+    socket.on("sendMessage", async (message, callback) => {
+		const user = await getUser(socket.id);
+
+		try {
+			io.to(user.room).emit("message", { user: user.name, text: message });
+			io.to(user.room).emit("roomData", {
+				room: user.room,
+				users: getUsersInRoom(user.room),
+			});
+			callback();
+		} catch (err) {
+			console.log(err.message);
+		}
+	});
+
+	socket.on("disconnect", () => {
+		const user = removeUser(socket.id);
+		console.log("disconnected");
+
+		if (user) {
+			io.to(user.room).emit("message", {
+				user: "admin",
+				text: `${user.name} has just left!`,
+			});
+		}
+	});
+})
+
+server.listen(PORT, () => {
+	console.log(`Server has started on port ${PORT}...`);
+});
